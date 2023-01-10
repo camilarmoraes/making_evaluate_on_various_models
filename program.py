69da6cfeb67bbdda4d16f14a783e7a5a3030b4d4
Camila Ribeiro de Moraes <72588121+camilarmoraes@users.noreply.github.com>
2023-01-10 17:45:52 -0300

    Update program.py

--- program.py
+++ program.py
@@ -5,12 +5,14 @@ import numpy as np
 
 
 #Array for all the models to load
-models = [0,0,0,0,0,0,0,0,0,0]
+models = []
 #len(models)
 
 
 for i in range(10):
-    models[i] = tf.keras.models.load_model(f'models{i+1}.h5')
+    models.append(tf.keras.models.load_model(f'models{i+1}.h5'))
+
+for i in range(len(models):
     loss, acc = models[i].evaluate(input_test, target_test, verbose=0)
     #print(f"Loss {i+1} ={loss}  ACC {i+1} = {acc} ")
     with open("results.txt","a") as archive:
